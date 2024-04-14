55927e10e935875c1468ffc33176662a8b91dac2
Davit Narimanidze <davit.narimanidze408@ens.tsu.edu.ge>
2024-04-14 21:31:36 +0400

    feat: implement Particle struct

--- src/main.c
+++ src/main.c
@@ -1,12 +1,52 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include "raylib.h"
+#include "raymath.h"
+
+const float NODE_RADIUS = 5.0;
+
+typedef struct {
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 acceleration;
+    float mass;
+} Particle;
+
+Particle ParticleCreate(Vector2 position, Vector2 velocity, Vector2 acceleration, float mass)
+{
+    Particle particle = {position, velocity, acceleration, mass};
+    return particle;
+}
+
+void ParticleDraw(Particle *particle)
+{
+    DrawCircleV(particle->position, NODE_RADIUS, WHITE);
+}
+
+void ParticlePositionUpdate(Particle *particle, double dt)
+{
+    Vector2 position_displacement = Vector2Scale(particle->velocity, dt);
+    particle->position = Vector2Add(particle->position, position_displacement); 
+}
+
+void ParticleVelocityUpdate(Particle *particle, double dt)
+{
+    Vector2 velocity_displacement = Vector2Scale(particle->acceleration, dt);
+    particle->velocity = Vector2Add(particle->velocity, velocity_displacement);
+}
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
 int main(int _argc, char *_argv[]) 
 {
+    Particle particle1 = ParticleCreate
+        (
+            (Vector2) {69.0, 220.0},
+            (Vector2) {69, 42},
+            (Vector2) {0.0, 191.0},
+            69.0
+        );
+
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Enjhin");
     SetTargetFPS(60);
     SetWindowMonitor(0);
@@ -15,33 +55,20 @@ int main(int _argc, char *_argv[])
     ClearBackground(BLACK);
     EndDrawing();
 
-    Vector2 prev_mouse_pos;
-    Vector2 mouse_pos;
+    double time_now = 0;
+    double time_prev;
 
     while (!WindowShouldClose())
     {
+        time_prev = time_now;
+        time_now = GetTime();
+        double dt = time_now - time_prev;
+
         BeginDrawing();
-
-        if(IsMouseButtonPressed(0))
-        {
-            mouse_pos = GetMousePosition();
-            prev_mouse_pos = mouse_pos;
-        }
-
-        if(IsMouseButtonDown(0))
-        {
-            mouse_pos = GetMousePosition();
-            DrawCircleV(mouse_pos, 5, RED);
-            DrawLineEx(prev_mouse_pos, mouse_pos, 10.0f, RED);
-            //            DrawLineBezier(prev_mouse_pos, mouse_pos, 10.0f, RED);
-            prev_mouse_pos = mouse_pos;
-        }
-
-        if(IsMouseButtonDown(1))
-        {
-            ClearBackground(BLACK);
-        }
-
+        ClearBackground(BLACK);
+        ParticleVelocityUpdate(&particle1, dt);
+        ParticlePositionUpdate(&particle1, dt);
+        ParticleDraw(&particle1);
         EndDrawing();
     }
     return 0;
