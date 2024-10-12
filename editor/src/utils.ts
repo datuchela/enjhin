9a54949fcbc6316d35abb7448d6cb1898de15839
Davit Narimanidze <davit.narimanidze408@ens.tsu.edu.ge>
2024-10-12 19:21:06 +0400

    chore: remove editor directory

--- editor/src/utils.ts
+++
@@ -1,9 +0,0 @@
-import { redrawCanvas } from "./canvas.js";
-import { refreshButtons } from "./dom.js";
-import { type State } from "./types.js";
-
-export function redrawAll(state: State) {
-  redrawCanvas(state);
-  refreshButtons(state);
-}
-
